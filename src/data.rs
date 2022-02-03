abf2d63766c1ec336b677e6a3603d94d6f4fe852
Avimitin <avimitin@gmail.com>
2022-02-03 23:39:14 +0800

    feat: implement clearurl data parser
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ src/data.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Providers {
+    providers: HashMap<String, Domain>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Domain {
+    url_pattern: String,
+    complete_provider: bool,
+    rules: Vec<String>,
+    referral_marketing: Vec<String>,
+    raw_rules: Vec<String>,
+    exceptions: Vec<String>,
+    redirections: Vec<String>,
+    force_redirection: bool,
+}
+
+#[allow(dead_code)]
+pub fn load_data(path: &str) -> Providers {
+    let path = Path::new(path);
+    let mut file = File::open(&path).unwrap();
+
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+
+    serde_json::from_str(&buffer).unwrap()
+}
+
+#[test]
+fn test_load_data() {
+    assert_ne!(0, load_data("./rules/data.min.json").providers.len());
+}
