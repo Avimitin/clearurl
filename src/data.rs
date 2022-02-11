2f618cd9e264055f75ff904aadcc8fa086345744
Avimitin <avimitin@gmail.com>
2022-02-11 23:10:00 +0800

    docs: add document for clearurl API
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/data.rs
+++ src/data.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+// Rule for single domain
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DomainConfig {
     pub match_sub: bool,
@@ -13,11 +14,17 @@ pub struct DomainConfig {
     pub rules: Vec<String>,
 }
 
+// A parsed domain rule set
 pub struct Domains {
     data: HashMap<String, DomainConfig>,
 }
 
 impl Domains {
+    /// Load rules for domain from given file. The file must be in toml format.
+    ///
+    /// # Errors
+    ///
+    /// This function return error when IO fail or parse progress fail.
     pub fn load_from_file(path: &str) -> Result<Domains> {
         let path = Path::new(path);
         let mut file = File::open(&path)?;
@@ -30,6 +37,18 @@ impl Domains {
         Ok(Domains { data })
     }
 
+    /// Get return the rule for given domain
+    ///
+    /// # Example
+    /// ```
+    /// use clearurl::Domains;
+    ///
+    /// let domain = "b23.tv";
+    /// let domain_ruleset = Domains::load_from_file("path to file");
+    /// let domain_rule = domain_ruleset.get(domain).unwrap();
+    ///
+    /// assert!(domain_rule.should_redirect);
+    /// ```
     pub fn get(&self, key: &str) -> Option<&DomainConfig> {
         self.data.get(key)
     }
