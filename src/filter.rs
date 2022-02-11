4ad262f5833689548254ac92255582619006570c
Avimitin <avimitin@gmail.com>
2022-02-11 17:04:28 +0800

    feat!/lib: add redirection support
    BREAKING CHANGE:
      * rename public API crate::filter::filter() to filter::clear()
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/filter.rs
+++ src/filter.rs
@@ -3,38 +3,60 @@ use anyhow::{bail, Context, Result};
 use url::form_urlencoded;
 use url::Url;
 
-pub fn filter<'a>(domains: &Domains, url: &'a mut Url) -> Result<&'a Url> {
+
+pub async fn clear(domains: &Domains, url: &mut Url) -> Result<Url> {
+    remove_query(domains, url).await
+}
+
+#[async_recursion::async_recursion]
+async fn remove_query(domains: &Domains, url: &mut Url) -> Result<Url> {
+    // get domain from url
     let domain = url.domain();
     if domain.is_none() {
-        bail!("this is an invalid URL")
+        bail!("invalid url: {}", url.as_str())
     }
     let domain = domain.unwrap();
-    let rule = domains
+
+    // get rule by domain
+    let mut domain_rule = domains
         .get(domain)
         .context(format!("no rule for domain: <{}>", domain))?;
 
-    if rule.rules.is_empty() {
+    // if the domain require redirect
+    if domain_rule.should_redirect {
+        let mut final_url = get_final_url(url.as_str()).await.context(format!(
+            "fail to make redirection for domain {}",
+            url.as_str()
+        ))?;
+        return remove_query(domains, &mut final_url).await;
+    }
+
+    if domain_rule.rules.is_empty() {
         bail!("no rule for domain: <{}>", domain)
     }
 
-    let rule = &rule.rules;
+    let blacklist = &domain_rule.rules;
 
     // take a copy of the query string for later use
-    let ori_query = url.query();
-    if ori_query.is_none() {
-        return Ok(url);
+    let ori_queries = url.query();
+    // if no query behind, return domain back immediately
+    if ori_queries.is_none() {
+        return Ok(url.to_owned());
     }
-    // copy the original query string
-    let ori_query = url.query().unwrap().to_string();
-    let ori_query = form_urlencoded::parse(ori_query.as_bytes());
+    // get the copy of the queries
+    let ori_queries = ori_queries.unwrap().to_string();
 
-    // clean the query string
+    // and parse it into pairs -> [(k, v)]
+    let ori_queries = form_urlencoded::parse(ori_queries.as_bytes());
+
+    // clean the original queries to get a clean url
     url.set_query(None);
 
-    for (key, val) in ori_query {
+    // append queries that are not in the blacklist
+    for (key, val) in ori_queries {
         let mut has_same = false;
-        for r in rule {
-            if key == r.as_str() {
+        for query in blacklist {
+            if key == query.as_str() {
                 has_same = true;
                 break;
             }
@@ -44,20 +66,33 @@ pub fn filter<'a>(domains: &Domains, url: &'a mut Url) -> Result<&'a Url> {
         }
     }
 
-    Ok(url)
+    Ok(url.to_owned())
 }
 
-#[test]
-fn test_filter() {
+async fn get_final_url(url: &str) -> Result<Url> {
+    let resp = reqwest::get(url).await?;
+
+    Ok(resp.url().to_owned())
+}
+
+#[tokio::test]
+async fn test_filter() {
     let data =
         crate::data::Domains::load_from_file("./rules.toml").expect("fail to read rules.toml");
+
+    // test normal rule
     let mut url = Url::parse(
         "https://twitter.com/CiloRanko/status/1478401918792011776?s=20&t=AVPOmNLtaozrA0Ccp6DyAw",
     )
     .unwrap();
-    let url = filter(&data, &mut url).unwrap();
+    let url = clear(&data, &mut url).await.unwrap();
     assert_eq!(
         url.as_str(),
         "https://twitter.com/CiloRanko/status/1478401918792011776"
     );
+
+    // test redirection
+    let mut url = Url::parse("https://b23.tv/Mf6vJOp").unwrap();
+    let url = clear(&data, &mut url).await.unwrap();
+    assert_eq!(url.as_str(), "https://www.bilibili.com/video/BV14L4y1x7Hn?p=1");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,5 @@
 
 pub mod data;
 pub mod filter;
+
+pub use filter::clear;
