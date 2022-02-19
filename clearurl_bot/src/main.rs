ff4f3add2f25294825a628dde394877a78266606
Avimitin <avimitin@gmail.com>
2022-02-19 22:25:01 +0800

    N'bot: implement with the new API
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- clearurl_bot/src/main.rs
+++ clearurl_bot/src/main.rs
@@ -1,16 +1,15 @@
-use clearurl::data;
-use clearurl::filter;
+use clearurl::UrlCleaner;
 use lazy_static::lazy_static;
 use log::{error, info};
 use teloxide::prelude2::*;
+use std::sync::Arc;
 
 lazy_static! {
     static ref HTTP_REGEX_MATCH_RULE: regex::Regex = regex::Regex::new(
         r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)",
     )
     .unwrap();
-    // TODO: use portable path for rules file
-    static ref RULES:data::Domains = data::Domains::load_from_file("../rules.toml").unwrap();
+    static ref CLEANER: Arc<UrlCleaner> = Arc::new(UrlCleaner::from_file("../rules.toml").unwrap());
 }
 
 #[tokio::main]
@@ -23,18 +22,13 @@ async fn main() {
     let bot = Bot::from_env().auto_send();
 
     teloxide::repls2::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+        let cleaner = Arc::clone(&CLEANER);
         let text = message.text().unwrap_or("");
         let capture = filter_domain(text);
         let mut buffer = String::new();
         for cap in capture {
-            let mut url = match url::Url::parse(&cap[1]) {
-                Ok(u) => u,
-                Err(e) => {
-                    error!("Fail to parse url: {} from capture. Error: {}", &cap[1], e);
-                    return respond(());
-                }
-            };
-            let url = match filter::clear(&RULES, &mut url).await {
+            let url = &cap[1];
+            let url = match cleaner.clear(url).await {
                 Ok(u) => u,
                 Err(e) => {
                     error!("{}", e);
