561e2b783353f0336c2db42f141a768924d72bc9
Avimitin <avimitin@gmail.com>
2022-02-24 22:37:21 +0800

    [RFT] bot: split dispatcher builder to functions
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- clearurl_bot/src/bot.rs
+++ clearurl_bot/src/bot.rs
@@ -1,74 +1,16 @@
-use teloxide::{dispatching2::UpdateFilterExt, prelude2::*, types::Update};
-use log::{error, info};
+use clearurl::UrlCleaner;
+use log::error;
+use std::sync::Arc;
+use teloxide::{dispatching2::UpdateFilterExt, prelude2::*, types::Update, RequestError};
 
-#[allow(dead_code)]
-async fn run() {
-    teloxide::enable_logging!();
-    log::info!("Starting bot...");
-
-    let bot_config = Config {
-        token: String::from("test token"),
-        enable_groups: vec![123456, 7891011],
-    };
-
-    let bot = Bot::new(&bot_config.token).auto_send();
-    let http_regex_rule = Box::new(regex::Regex::new(
-        r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)",
-    )
-    .expect("Illegal http regex rule"));
-
-    let update_handler = Update::filter_message().branch(
-        dptree::filter(|msg: &Message, cfg: &Config| {
-            msg.text().is_some() && cfg.is_enabled_group(msg.chat_id())
-        })
-        .endpoint(
-            |msg: Message, bot: AutoSend<Bot>, cleaner: clearurl::UrlCleaner, regex: regex::Regex | async move {
-                let caps = regex.captures_iter(msg.text().unwrap_or(""));
-                let mut buffer = String::new();
-                for cap in caps {
-                    let url = &cap[1];
-                    let url = match cleaner.clear(url).await {
-                        Ok(u) => u,
-                        Err(e) => {
-                            error!("{}", e);
-                            return respond(());
-                        }
-                    };
-                    buffer.push_str(url.as_str());
-                    buffer.push('\n');
-                }
-
-                if !buffer.is_empty() {
-                    let resp = bot.send_message(msg.chat_id(), buffer).await;
-                    if let Err(e) = resp {
-                        error!("{}", e);
-                    }
-                }
-                respond(())
-            },
-        ),
-    );
-
-    Dispatcher::builder(bot, update_handler)
-        .dependencies(dptree::deps![bot_config, http_regex_rule])
-        .default_handler(|upd| async move {
-            log::warn!("Unhandle update from chat {}", upd.chat().unwrap().id)
-        })
-        .error_handler(LoggingErrorHandler::with_custom_text("Fail to handle"))
-        .build()
-        .setup_ctrlc_handler()
-        .dispatch()
-        .await;
-}
-
+#[derive(Clone)]
 struct Config {
-    token: String,
-    enable_groups: Vec<i64>,
+    enable_groups: Arc<Vec<i64>>,
 }
 
 impl Config {
     pub fn is_enabled_group(&self, g: i64) -> bool {
-        for group in &self.enable_groups {
+        for group in self.enable_groups.iter() {
             if g == *group {
                 return true;
             }
@@ -76,4 +18,77 @@ impl Config {
 
         false
     }
+}
+
+async fn handle_link_message(
+    msg: Message,
+    bot: AutoSend<Bot>,
+    cleaner: Arc<UrlCleaner>,
+    regex: Arc<regex::Regex>,
+) -> Result<(), RequestError> {
+    let caps = regex.captures_iter(msg.text().unwrap_or(""));
+    let mut buffer = String::new();
+    for cap in caps {
+        let url = &cap[1];
+        let url = match cleaner.clear(url).await {
+            Ok(u) => u,
+            Err(e) => {
+                error!("{}", e);
+                return respond(());
+            }
+        };
+        buffer.push_str(url.as_str());
+        buffer.push('\n');
+    }
+
+    if !buffer.is_empty() {
+        let resp = bot.send_message(msg.chat_id(), buffer).await;
+        if let Err(e) = resp {
+            error!("{}", e);
+        }
+    }
+    respond(())
+}
+
+fn build_runtime() -> (AutoSend<Bot>, Arc<UrlCleaner>, Arc<regex::Regex>) {
+    let bot = Bot::from_env().auto_send();
+    let cleaner = Arc::new(UrlCleaner::from_file("../rules.toml").unwrap());
+    let http_regex_rule = Arc::new(
+        regex::Regex::new(
+            r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)",
+        )
+        .expect("Illegal http regex rule"),
+    );
+
+    (bot, cleaner, http_regex_rule)
+}
+
+fn build_handler() -> Handler<'static, DependencyMap, Result<(), RequestError>> {
+    Update::filter_message().branch(
+        dptree::filter(|msg: Message, cfg: Config| {
+            msg.text().is_some() && cfg.is_enabled_group(msg.chat_id())
+        })
+        .endpoint(handle_link_message),
+    )
+}
+
+pub async fn run() {
+    teloxide::enable_logging!();
+    dotenv::dotenv().ok();
+    log::info!("Starting bot...");
+
+    let bot_config = Config {
+        enable_groups: Arc::new(vec![]),
+    };
+
+    let (bot, cleaner, http_regex_rule) = build_runtime();
+
+    Dispatcher::builder(bot, build_handler())
+        .dependencies(dptree::deps![bot_config, http_regex_rule, cleaner])
+        .default_handler(|_| async move {})
+        .error_handler(LoggingErrorHandler::with_custom_text("Fail to handle"))
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
--- clearurl_bot/src/main.rs
+++ clearurl_bot/src/main.rs
@@ -1,62 +1,6 @@
-use clearurl::UrlCleaner;
-use lazy_static::lazy_static;
-use log::{error, info};
-use teloxide::{RequestError, prelude2::*};
-use std::sync::Arc;
-
 mod bot;
-
-lazy_static! {
-    static ref HTTP_REGEX_MATCH_RULE: regex::Regex = regex::Regex::new(
-        r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)",
-    )
-    .unwrap();
-    static ref CLEANER: Arc<UrlCleaner> = Arc::new(UrlCleaner::from_file("../rules.toml").unwrap());
-}
-
-async fn handle_text(message: Message, bot: AutoSend<Bot>) -> Result<(), RequestError> {
-    let cleaner = Arc::clone(&CLEANER);
-    let text = message.text().unwrap_or("");
-    let capture = filter_domain(text);
-    let mut buffer = String::new();
-    for cap in capture {
-        let url = &cap[1];
-        let url = match cleaner.clear(url).await {
-            Ok(u) => u,
-            Err(e) => {
-                error!("{}", e);
-                return respond(());
-            }
-        };
-        buffer.push_str(url.as_str());
-        buffer.push('\n');
-    }
-
-    if !buffer.is_empty() {
-        let resp = bot.send_message(message.chat_id(), buffer).await;
-        if let Err(e) = resp {
-            error!("{}", e);
-        }
-    }
-    respond(())
-}
-
-async fn run() {
-    dotenv::dotenv().ok();
-
-    teloxide::enable_logging!();
-    info!("Starting clearurl_bot...");
-
-    let bot = Bot::from_env().auto_send();
-
-    teloxide::repls2::repl(bot, handle_text).await;
-}
 
 #[tokio::main]
 async fn main() {
-    run().await
-}
-
-fn filter_domain(text: &str) -> regex::CaptureMatches {
-    HTTP_REGEX_MATCH_RULE.captures_iter(text)
+    bot::run().await
 }
