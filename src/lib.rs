fec95170eae37de4327aee8df3f8554484d2fc60
Avimitin <dev@avimit.in>
2023-11-05 15:56:54 +0800

    clearurl: refactor the clearurl API
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,9 @@ async fn main() {
 }
 */
 
-mod filter;
 mod rules;
+
+use std::sync::Arc;
 
 use url::Url;
 
@@ -46,7 +47,24 @@ use url::Url;
 /// corresbonding function together.
 pub struct UrlCleaner {
     /// ruleset contains rules for domain
-    rt: rules::RuntimeRules,
+    rules: rules::Rules,
+    http_client: reqwest::Client,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum UrlCleanError {
+    #[error("fail to parse input URL")]
+    UrlParseError(#[from] url::ParseError),
+    #[error("URL have no domain")]
+    NoDomain,
+    #[error("URL doesn't have any query")]
+    NoQuery,
+    #[error("fail to do a redirect when cleaning URL")]
+    RedirectFail(#[from] reqwest::Error),
+    #[error("no rule match for this URL")]
+    NoMatchRule,
+    #[error("this URL is already cleared")]
+    NothingToClear,
 }
 
 impl UrlCleaner {
@@ -55,23 +73,162 @@ impl UrlCleaner {
     /// # Error
     ///
     /// Return error when IO fail or meeting unexpected format.
-    pub fn from_file(path: &str) -> anyhow::Result<UrlCleaner> {
+    pub fn from_file(path: &str) -> Result<UrlCleaner, reqwest::Error> {
+        let mut http_client = reqwest::ClientBuilder::new();
+
+        if let Ok(var) = std::env::var("HTTP_PROXY") {
+            http_client = http_client.proxy(reqwest::Proxy::http(var)?);
+        };
+
+        if let Ok(var) = std::env::var("HTTPS_PROXY") {
+            http_client = http_client.proxy(reqwest::Proxy::https(var)?);
+        };
+
+        if let Ok(var) = std::env::var("ALL_PROXY") {
+            http_client = http_client.proxy(reqwest::Proxy::all(var)?);
+        };
+
         Ok(UrlCleaner {
-            rt: rules::parse(std::path::Path::new(path)),
+            rules: rules::parse(std::path::Path::new(path)),
+            http_client: http_client.build()?,
         })
     }
 
-    /// The clear function accepct a url string and try to parse it into a new
-    /// Url struct without tracking queries.
-    pub async fn clear(&self, url: &str) -> Option<Url> {
-        match filter::clear(url, &self.rt).await {
-            Ok(url) => Some(url),
-            Err(e) => {
-                tracing::trace!("Error occur when filtering url {}: {}", url, e);
-                None
+    /// Clear the query of the given URL by pre-define rules.
+    ///
+    /// # Error
+    ///
+    /// Return error if:
+    ///     * url is invalid
+    ///     * no rule found for the given URL and default rule is also not found
+    ///     * no query behind the url
+    ///     * rule for the given url is empty
+    pub async fn clear(&self, url: &str) -> Result<Url, UrlCleanError> {
+        let mut url = Url::parse(url)?;
+
+        let get_rule = {
+            #[inline]
+            |domain: &str| -> Arc<rules::Rule> {
+                self.rules
+                    .get(domain)
+                    .cloned()
+                    .unwrap_or(self.rules.get("default").cloned().unwrap())
+            }
+        };
+
+        let mut domain = url.domain().ok_or_else(|| UrlCleanError::NoDomain)?;
+        let mut rule = get_rule(domain);
+
+        if rule.redirect {
+            url = self.http_client.head(url).send().await?.url().clone();
+            domain = url.domain().unwrap();
+            rule = get_rule(domain);
+        }
+
+        if url.query().is_none() {
+            return Err(UrlCleanError::NoQuery);
+        }
+
+        if url.query().unwrap().is_empty() {
+            return Err(UrlCleanError::NoQuery);
+        }
+
+        if rule.rules.is_empty() {
+            return Err(UrlCleanError::NoMatchRule);
+        }
+
+        let mut new_url = url.clone();
+        new_url.set_query(None);
+        url.query_pairs()
+            .filter(|(k, _)| {
+                let mut is_clean = true;
+                for re in &rule.rules {
+                    if re.is_match(k) {
+                        is_clean = false;
+                        break;
+                    }
+                }
+                is_clean
+            })
+            .for_each(|(k, v)| {
+                new_url.query_pairs_mut().append_pair(&k, &v);
+            });
+
+        if let Some(query) = new_url.query() {
+            if query == url.query().unwrap() {
+                return Err(UrlCleanError::NothingToClear);
             }
         }
+
+        Ok(new_url)
     }
 }
 
-// vim: tw=80 fo+=t
+#[tokio::test]
+async fn test_filter() {
+    let cleaner = UrlCleaner::from_file("./rules.toml").unwrap();
+
+    // * test normal rule
+    let url = cleaner.clear(
+        "https://www.bilibili.com/video/BV18c411f75F/?-Arouter=story&buvid=XUA26FCA524D1B63D221F4D6DE86A9EDCC84A&from_spmid=tm.recommend.0.0&is_story_h5=true&mid=7guN1WLkkGNxM7XOufwKvQ%3D%3D&p=1&plat_id=163&share_from=ugc&share_medium=android&share_plat=android&share_session_id=0237255d-d385-49df-861f-b303e20bef5b&share_source=COPY&share_tag=s_i&spmid=main.ugc-video-detail-vertical.0.0&timestamp=1699071016&unique_k=hkeZH3o&up_id=1343541951&t=1230",
+    )
+    .await
+    .unwrap();
+    assert_eq!(
+        url.as_str(),
+        "https://www.bilibili.com/video/BV18c411f75F/?p=1&t=1230"
+    );
+
+    // * test redirection
+    let url = cleaner.clear("https://b23.tv/acFF8P0").await.unwrap();
+    assert_eq!(
+        url.as_str(),
+        // normal queries will be kept
+        "https://www.bilibili.com/video/BV1nY411r7o1/?p=1"
+    );
+
+    // * test regex
+    let url = cleaner.clear(
+        "https://www.amazon.com/b/?node=226184&ref_=Oct_d_odnav_d_1077068_1&pd_rd_w=ZjwFQ&pf_rd_p=0f6f8a08-29ea-497e-8cb4-0ccf91422740&pf_rd_r=YMQ5XPAZHYHV77HCENY7&pd_rd_r=27c502f2-951f-4a8c-9478-381febc5e5bc&pd_rd_wg=NxaQ1",
+    )
+    .await
+    .unwrap();
+    assert_eq!(
+        url.as_str(),
+        // normal queries will be kept
+        "https://www.amazon.com/b/?node=226184"
+    );
+
+    // * test import
+    let url = cleaner.clear(
+        "https://post.m.smzdm.com/p/aoxzv08r/?zdm_ss=iOS__hczZ7LgGInW%2BUXtAcwyZGSVdJqcPFvT98aEipRx9K%2BPOH7mQ0YGD3w%3D%3D&from=other",
+    )
+    .await
+    .unwrap();
+    assert_eq!(
+        url.as_str(),
+        // normal queries will be kept
+        "https://post.m.smzdm.com/p/aoxzv08r/",
+    );
+
+    // * test default
+    let url = cleaner
+        .clear("https://example.com?utm_source=ios")
+        .await
+        .unwrap();
+    assert_eq!(
+        url.as_str(),
+        // normal queries will be kept
+        "https://example.com/",
+    );
+
+    // * test default
+    let url = cleaner.clear("https://www.youtube.com/watch?v=FqT_Ofd54fo&list=PLXSyc11qLa1YfSbP700GXf5VSvpVm2zMO&index=42")
+        .await;
+    match url {
+        Err(UrlCleanError::NothingToClear) => {}
+        _ => {
+            panic!("URL doesn't return error NothingToClear")
+        }
+    }
+}
--- src/rules.rs
+++ src/rules.rs
@@ -1,29 +1,15 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-
-// generate default value for the `sub` field
-fn default_subdomain() -> Option<Vec<String>> {
-    None
-}
+use std::sync::Arc;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ConfigData {
-    #[serde(default = "default_subdomain")]
+    #[serde(default)]
     sub: Option<Vec<String>>,
     #[serde(default)]
     redirect: bool,
     #[serde(default)]
     ban: Vec<String>,
-}
-
-/// RuntimeRules expanded the configuration to actually needed rules.
-#[derive(Debug)]
-pub struct RuntimeRules(HashMap<String, Rule>);
-
-impl RuntimeRules {
-    pub fn get(&self, base: &str) -> Option<&Rule> {
-        self.0.get(base)
-    }
 }
 
 /// Represent rule for a single domain.
@@ -33,41 +19,8 @@ pub struct Rule {
     pub rules: Vec<regex::Regex>,
 }
 
-/// Expand the configuration to runtime data.
-///
-/// # Error
-///
-/// Panic if the the `ban` field is a invalid regex rule.
-fn expand(config: HashMap<String, ConfigData>) -> RuntimeRules {
-    let mut rules = HashMap::new();
-    for (base, data) in config {
-        let r = Rule {
-            redirect: data.redirect,
-            rules: data
-                .ban
-                .into_iter()
-                .map(|re| {
-                    // Use `unwrap_or_else()` instead of `expect` to avoid overhead
-                    regex::Regex::new(&re)
-                        .unwrap_or_else(|error| panic!("Invalid regexp: {re}\n\n\t{error}"))
-                })
-                .collect(),
-        };
-
-        if let Some(sub) = data.sub {
-            for s in sub {
-                // FIXME: Can we clone a reference not the data.
-                // Try use Arc? We should take care of the self-reference bug.
-                // Also we are going to be a async lib, Send + Sync is also required.
-                rules.insert(format!("{s}.{base}"), r.clone());
-            }
-        } else {
-            rules.insert(base, r);
-        }
-    }
-
-    RuntimeRules(rules)
-}
+/// Rules is a KV map with K as full-formed URL, V as clean rules.
+pub type Rules = HashMap<String, Arc<Rule>>;
 
 /// Parse rules configuration file from given `location`.
 ///
@@ -77,27 +30,36 @@ fn expand(config: HashMap<String, ConfigData>) -> RuntimeRules {
 ///   * fail to read the file content
 ///   * fail to parse content into expected struct
 ///   * regexp is invalid
-pub fn parse(location: &std::path::Path) -> RuntimeRules {
-    let content = std::fs::read(location).unwrap_or_else(|error| {
-        // HINT: Some OS might allow non-UTF-8 string as file path, so location.to_str().unwrap()
-        // might panic when someone compile this program on those OS.
-        panic!("fail to read from {}: {error}", location.to_str().unwrap())
-    });
+pub fn parse(location: &std::path::Path) -> Rules {
+    let content = std::fs::read_to_string(location)
+        .unwrap_or_else(|error| panic!("fail to read from {location:?}: {error}"));
 
-    let config: HashMap<String, ConfigData> = toml::from_slice(&content).unwrap_or_else(|error| {
-        // Panic with full content when user set RUST_LOG=TRACE
-        let trace = std::env::var("RUST_LOG");
-        if let Ok(var) = trace {
-            if var.to_uppercase() == "TRACE" {
-                panic!(
-                    "fail to parse rules content: {error}\n\nFull Content: {:#?}",
-                    String::from_utf8(content)
-                )
-            }
+    let config: HashMap<String, ConfigData> = toml::from_str(&content)
+        .unwrap_or_else(|error| panic!("fail to parse {location:?} into rules: {error}"));
+
+    let mut rules = HashMap::new();
+    config.into_iter().for_each(|(base, data)| {
+        let rule = Arc::new(Rule {
+            redirect: data.redirect,
+            rules: data
+                .ban
+                .into_iter()
+                .map(|re| {
+                    // Use `unwrap_or_else()` instead of `expect` to avoid overhead
+                    regex::Regex::new(&re).unwrap_or_else(|error| {
+                        panic!("Invalid regexp: '{re}' for URL: {base}\n\nError: {error}")
+                    })
+                })
+                .collect(),
+        });
+        if let Some(sub) = data.sub {
+            sub.into_iter().for_each(|sub_domain| {
+                rules.insert(format!("{sub_domain}.{base}"), Arc::clone(&rule));
+            })
+        } else {
+            rules.insert(base, rule);
         }
-
-        panic!("fail to parse rules content: {error}")
     });
 
-    expand(config)
+    rules
 }
